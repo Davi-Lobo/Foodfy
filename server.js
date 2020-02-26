e7287dbddd476e35e379ae380ff79f1daa1eaf50
Davi Lobo <davi.lobo.gs@gmail.com>
2020-02-26 14:35:15 -0300

    Dynamic data and browser-sync added

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const nunjucks = require('nunjucks');
 
+const recipes = require('./data');
+
 const server = express();
 
 server.set("view engine", "njk");
@@ -12,11 +14,11 @@ nunjucks.configure("views", {
  });
 
 server.get("/", function(req, res) {
-    return res.render("index");
+    return res.render("index", { items : recipes });
 });
 
 server.get("/recipes", function(req, res) {
-    return res.render("recipes");
+    return res.render("recipes", { items : recipes });
 });
 
 server.get("/about", function(req, res) {
